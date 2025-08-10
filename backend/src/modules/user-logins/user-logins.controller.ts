fc2b60ef2a5680c72aff62aae139ee36863ca73b
enricolund <enrico.lund@gmail.com>
2025-08-10 15:41:02 +0300

    remove user-login controller, renaming files

--- backend/src/modules/user-logins/user-logins.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-// import { UserLoginsService } from './user-logins.service';
-
-@Controller('user-logins')
-export class UserLoginsController {
-  // constructor(private readonly userLoginsService: UserLoginsService) {}
-}
--- backend/src/modules/user-logins/user-logins.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserLoginsService } from './user-logins.service';
-import { UserLoginsController } from './user-logins.controller';
-
-@Module({
-  controllers: [UserLoginsController],
-  providers: [UserLoginsService],
-})
-export class UserLoginsModule {}
