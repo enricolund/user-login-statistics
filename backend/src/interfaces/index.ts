026e0469d951c210db40f99b9ed0adbf182cc4ec
enricolund <enrico.lund@gmail.com>
2025-08-10 17:39:29 +0300

    cleanup old structure files

--- backend/src/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-// Re-export all interfaces from separated files
-export * from './user-login.interface';
-export * from './stats.interface';
-export * from './cache.interface';
-export * from './websocket.interface';
-export * from './repository.interface';
--- backend/src/websocket/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './interfaces/websocket.interfaces';
-export * from './services/client-manager.service';
-export * from './services/message-handler.service';
-export * from './services/stats-provider.service';
-export * from './websocket-server';
