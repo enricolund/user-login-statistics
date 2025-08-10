125b3a5486f49cef2ecb16202dd9e216aa7ae298
enricolund <enrico.lund@gmail.com>
2025-08-10 15:57:51 +0300

    cleanup old files

--- backend/src/interfaces/RepositoryInterfaces.ts
+++
@@ -1,8 +0,0 @@
-import { DeviceStats, RegionStats, BrowserStats, SessionDurationStats } from './stats.interface';
-
-export interface IStatsRepository {
-  getDeviceTypeCounts(): Promise<DeviceStats[]>;
-  getRegionCounts(): Promise<RegionStats[]>;
-  getBrowserCounts(): Promise<BrowserStats[]>;
-  getSessionDurationStatsOptimized(): Promise<SessionDurationStats>;
-}
--- backend/src/interfaces/websocket.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IWebSocketBroadcaster {
-  broadcastToAll(message: any): Promise<void>;
-  getConnectionCount(): number;
-  start(port: number, path: string): void;
-  stop(): void;
-}
